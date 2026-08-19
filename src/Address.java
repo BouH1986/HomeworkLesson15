2941474bc9a93246c4dfec0ba545204a2e2c2efb
BouH1986 <jack-2006@mail.ru>
2026-08-19 09:23:48 +0400

    add override method hash and equals

--- src/Address.java
+++ src/Address.java
@@ -17,18 +17,21 @@ public class Address {
         return city;
     }
 
+    //переопределяем метод equals, чтобы можно было сравнивать два объекта Address по обоим полям
+    @Override
+    public boolean equals(Object obj) {
+        Address adr = (Address) obj;
+        return country.equals(adr.country) && city.equals(adr.city);
+    }
+
+    //при переопределении equals нужно обязательно переопределить метод hashCode,
+    // чтобы не было рассогласованности в этих двух методах,
+    // так как hashCode может возвращать хеш имени ячейки памяти
+
     //переопределяем метод hashCode, чтобы хеш объекта Address
     // считался и по полю страны и по полю города
     @Override
     public int hashCode() {
         return Objects.hash(country, city);
-    }
-
-    //переопределяем метод equals чтобы можно было сравнивать два объекта Address,
-    // не расписывая в условном операторе равенство обоих полей
-    @Override
-    public boolean equals(Object obj) {
-        Address adr = (Address) obj;
-        return country.equals(adr.country) && city.equals(adr.city);
     }
 }
